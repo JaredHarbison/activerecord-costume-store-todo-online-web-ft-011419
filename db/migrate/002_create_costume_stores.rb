0356e2114e4722d45401e153d450ae776b320ccc
Jared Harbison <jared.harbison@gmail.com>
2019-02-22 15:09:20 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,12 +1,12 @@
 class CreateCostumeStore <ActiveRecord::Migration[4.2]
-  def change 
-    create_table :costume_stores do |t| 
-      t.string :name 
+  def change
+    create_table :costume_stores do |t|
+      t.string :name
       t.string :location
       t.integer :costume_inventory
       t.integer :number_of_employees
-      t.boolean :still_in_business 
+      t.boolean :still_in_business
       t.timestamps
-    end 
-  end 
+    end
+  end
 end 
