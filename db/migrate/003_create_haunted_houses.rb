33a963a13cde1a196bfdf7d5692f086562d02210
Jared Harbison <jared.harbison@gmail.com>
2019-02-22 15:21:22 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -5,10 +5,10 @@ class CreateHauntedHouses <ActiveRecord::Migration[4.2]
       t.string :location
       t.string :theme
       t.float :price
-      t.boolean :friend_or_family
+      t.boolean :family_friendly
       t.datetime :opening_date
       t.datetime :closing_date
-      t.string :long_description
+      t.text :description 
       t.timestamps
     end
   end
